3c0a8a3ac20b0be15cdca6980d82c47eef243ce9
Nipun Sachinda <46530852+Nipun258@users.noreply.github.com>
2022-03-02 16:29:40 +0530

    device peview add to system

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,33 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
+
+//void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Nipun Sachinda'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('MY APP'),
+        ),
+        body: const Center(
+          child: Text('First App'),
+        ),
+      ),
     );
   }
 }
